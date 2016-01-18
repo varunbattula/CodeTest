4a65fd58f19b5aa3013551226a173b38ebf3c3f2
Varun Battula <varunbattula14@gmail.com>
2016-01-18 11:08:23 -0600

    Keys

--- stream_consumer.rb
+++ stream_consumer.rb
@@ -4,10 +4,10 @@ require './lib/tweet_data.rb'
 url = 'https://stream.twitter.com/1.1/statuses/sample.json'
 
 oauth_config = {
-    :consumer_key     => 'coByzQgUa78l6yE4JundNjJpH',
-    :consumer_secret  => 'UyCgLhJoZhpu5yY78U7FaMgwYpnEtoN67xtYEuPti6F9Sp12lP',
-    :access_token     => '4823257306-06RKVr3aHvfI8Y2WOhSUb1dijNiYsQLJD2Xra6V',
-    :access_token_secret => 'I96QXddVpzK8JdEPzO3xmU2zYqlU7tKoUsOF8vY5nTDsP'
+    :consumer_key     => '',
+    :consumer_secret  => '',
+    :access_token     => '',
+    :access_token_secret => ' '
     }
 
 tc = Twitter::Client.new(url,oauth_config)
